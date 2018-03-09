f808e64f732f9c8721f9408171d1a2c4270c16ef
firasBarrek <mohamedfiras.barrek@esprit.tn>
2018-03-09 03:05:50 +0100

    builds

---
+++ build (jerome)/settings-data.js
@@ -0,0 +1,1 @@
+window.EXTENSION_CONFIG = {"buildType":"dev","apiUrl":"http://localhost:5000/api/","serviceUrl":"http://localhost:5000/","appType":"chrome-extension","googleAnalytics":"UA-26026798-6"};
